bdebd57e82dac1e7b5daf3ec00e6207cad99a562
ZARIX <46120048+MohdXHassan@users.noreply.github.com>
2024-01-30 10:12:45 +0530

    Add files via upload

---
+++ Selenium-project/src/App.java
@@ -0,0 +1,75 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+   
+        
+       
+        System.getProperty("webdriver.chrome.driver", "/Users/mdhassan/Downloads/chrome-mac-arm64") ;
+        WebDriver driver = new ChromeDriver();
+
+       
+        //  // For opening a page :-
+        //  driver.get("https://www.google.com/");
+        
+        //Open the HTML page
+        driver.get("http://127.0.0.1:5500/src/Selenium.html");
+
+      
+        // Find the input element by its ID
+       
+
+        
+
+        // Enter a number into the input field
+     
+        // WebElement buttonElement = driver.findElement(By.xpath("//button[text()='Display Value']"));
+        WebElement buttonElement = driver.findElement(By.id("two"));
+    
+
+        buttonElement.click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        WebElement buttonElement1 = driver.findElement(By.id("mul"));
+      
+
+        buttonElement1.click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        WebElement buttonElement2 = driver.findElement(By.id("three"));
+      
+
+        buttonElement2.click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        WebElement buttonElement4 = driver.findElement(By.id("equal"));
+      
+
+        buttonElement4.click();
+        try {
+            Thread.sleep(200000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // try {
+        //     Thread.sleep(2000000);
+        // } catch (InterruptedException e) {
+        //     e.printStackTrace();
+        // }
+        // Close the browser
+        driver.quit();
+    }
+}
